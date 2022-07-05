1a1ba37e067167c990294f93e113ff74542ee74f
KhFazliddin <81314108+KhFazliddin@users.noreply.github.com>
2022-07-05 11:27:20 +0500

    filter the products

--- src/component/Product.js
+++ src/component/Product.js
@@ -6,60 +6,46 @@ import "react-slideshow-image/dist/styles.css";
 function Product() {
   const product = useSelector((state) => state.product.products);
   // const categories = useSelector((state) => state.product.categories);
-  const [datas, setDatas] = useState(product);
+
+  const [datas, setDatas] = useState(product.products);
   const dispatch = useDispatch();
   const handleAdd = (product) => {
     dispatch(add(product));
     console.log(handleAdd);
   };
 
+  const allCategories = product.products.map((item) => item.category);
+  const categories = ["all", ...new Set(allCategories)];
   const filterResult = (catItem) => {
-    const result = product.products.filter((curData) => {
-      return curData.category === catItem;
-    });
+    if (catItem === "all") {
+      setDatas(product.products);
+      return;
+    }
+
+    const result = product.products.filter(
+      (curData) => curData.category === catItem
+    );
     setDatas(result);
-    console.log(result);
   };
+  function brief(text) {
+    return text.slice(0, 15) + "...";
+  }
+
   return (
     <div className="grid">
       <div>
-        <button
-          className="categoryBtn"
-          onClick={() => filterResult("smartphones")}
-        >
-          smartphones
-        </button>
-        <button className="categoryBtn" onClick={() => filterResult("laptops")}>
-          laptops
-        </button>
-        <button
-          className="categoryBtn"
-          onClick={() => filterResult("fragrances")}
-        >
-          fragrances
-        </button>
-        <button
-          className="categoryBtn"
-          onClick={() => filterResult("skincare")}
-        >
-          skincare
-        </button>
-        <button
-          className="categoryBtn"
-          onClick={() => filterResult("groceries")}
-        >
-          groceries
-        </button>
-        <button
-          className="categoryBtn"
-          onClick={() => filterResult("home-decoration")}
-        >
-          home-decoration
-        </button>
-        <button className="categoryBtn">All</button>
+        {categories.map((item, index) => (
+          <button
+            onClick={() => filterResult(item)}
+            className="categoryBtn"
+            key={index}
+          >
+            {item}
+          </button>
+        ))}
       </div>
       <div className="productsWrapper">
-        {product.products.map((product, index) => {
+        {datas?.map((product, index) => {
           const { images, title, description, price } = product;
           return (
             <div className="card" key={index}>
@@ -73,20 +59,6 @@ function Product() {
             </div>
           );
         })}
-        {/* {datas.products?.map((item, id) => {
-          const { images, title, description, price } = item;
-          return (
-            <div className="card" key={id}>
-              <img src={images} alt="" />
-              <h3>{title}</h3>
-              <h4>{description}</h4>
-              <h5>${price}</h5>
-              <button onClick={() => handleAdd(product)} className="btn">
-                Add to cart
-              </button>
-            </div>
-          );
-        })} */}
       </div>
     </div>
   );
